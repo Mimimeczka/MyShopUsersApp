b5f46d8d356a945c2adeb85439fbc7d3b1026b02
Mimimeczka <justyna.zaczyk@onet.pl>
2021-05-12 14:45:12 +0200

    create methods: POST, PUT, DELETE, GET for User class

--- UsersApp/views.py
+++ UsersApp/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from .serializers import UserSerializer
+from rest_framework import viewsets
+from .models import User
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    # queryset = Product.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        user = User.objects.all()
+        return user
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = UserSerializer(instance)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        user = User.objects.create(
+            name=request.data['name'],
+            last_name=request.data['last_name'],
+            mail=request.data['mail'],
+            password=request.data['password']
+        )
+        serializer = UserSerializer(user, many=False)
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        user = self.get_object()
+        if request.data['name'] is not '':
+            user.name = request.data['name']
+        if request.data['last_name'] is not '':
+            user.last_name = request.data['last_name']
+        if request.data['mail'] is not '':
+            user.mail = request.data['mail']
+        if request.data['password'] is not '':
+            user.password = request.data['password']
+        user.save()
+        serializer = UserSerializer(user, many=False)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        user = self.get_object()
+        user.delete()
+        return Response('User removed')
