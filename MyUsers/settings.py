ee4026dbd34c9450fca9c37a5ffe3aa056e98f15
Mimimeczka <justyna.zaczyk@onet.pl>
2021-05-12 14:29:06 +0200

    create user url

--- MyUsers/settings.py
+++ MyUsers/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'UsersApp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- UsersApp/models.py
+++ UsersApp/models.py
@@ -6,3 +6,6 @@ class User(models.Model):
     last_name = models.CharField(max_length=50, null=False)
     mail = models.CharField(unique=True, null=False, max_length=50)
     password = models.CharField(null=False, max_length=50)
+
+    def __str__(self):
+        return f'{self.name} {self.last_name}'
